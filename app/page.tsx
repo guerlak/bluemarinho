90fca58bbfedbb2978367c4c7d4ea3c5b35286fa
Rafa Guerlak <guerlak@hotmail.com>
2024-03-11 17:37:43 -0300

    realease video and contact

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import { FaInstagram, FaYoutube } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="p-10">
+    <main className="p-10 lg:px-60">
 
       <h1 className="font-bold">BLUE MARINHO</h1>
 
-      <p className="my-7">Ecoando Clássicos com Originalidade: a Banda Blue Marinho oferece uma experiência única para os amantes do rock
+      <p className="my-7">Ecoando Clássicos com Originalidade: a Banda Blue Marinho oferece uma experiência única para os amantes do rock.
         Chega à cena carioca os Blue Marinho, um quinteto composto pela voz poderosa e envolvente de Paola Marinho e
         uma banda potente que apresenta uma rica mistura de releituras de clássicos do rock e blues dos anos 70 e 80.
       </p>
@@ -30,15 +30,27 @@ export default function Home() {
         Produtores de shows e amantes de uma boa música estão convidados a testemunhar esse show de rock cheio de personalidade.
       </p>
 
-      <p className="my-7">Para mais informações, entre em contato com:</p>
+      <p className="py-1">Shows e mais informações, entre em contato com:</p>
+      <p>Filipe: (21) 98844-4059</p>
+      <p>Mari: (21) 99744-8621</p>
+      <p>bandabluemarinho@gmail.com</p>
 
-      <p className="my-3">Siga-nos nas redes sociais:</p>
+
+      <br />
+
+      <p className="my-1">Siga-nos nas redes sociais:</p>
       <div className="flex">
         <a href="https://www.youtube.com/@BlueMarinho" target="_blank" rel="noopener noreferrer" className="p-2"><FaYoutube size={40} /></a>
         <a href="https://www.instagram.com/bandabluemarinho/" target="_blank" rel="noopener noreferrer" className="p-2"> <FaInstagram size={40} /></a>
       </div>
+      <br />
 
-      <h2>Prepare-se para uma viagem musical com os Blue Marinho. O rock nunca soou tão autêntico.</h2>
+      <q><strong>Prepare-se para uma viagem musical com os Blue Marinho. O rock nunca soou tão autêntico</strong>.</q>
+
+      <div className="flex justify-center my-10 b-1">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/kd57l2z8Pz8?si=nXMj9Ox2HBOpHBaU" title="YouTube video player" frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
 
     </main>
   );
