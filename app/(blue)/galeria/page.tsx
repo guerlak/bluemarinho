93b08488e171967580097e8fa1a3cc4d84865f94
Rafa Guerlak <guerlak@hotmail.com>
2024-03-16 00:25:29 -0300

    fix: key prop galeria 2

--- app/(blue)/galeria/page.tsx
+++ app/(blue)/galeria/page.tsx
@@ -19,8 +19,8 @@ function Galeria() {
 
             <div className="flex flex-wrap justify-center">
 
-                {pics.map((p, i) => (
-                    <button className="p-2">
+                {pics.map((p, i: any) => (
+                    <button className="p-2" key={i}>
                         <Image className="rounded-md" src={pics[i]} key={i} alt="Fotos de estudio por Pedro Colo" width={400} height={500} />
                     </button>
 
