89c54ad4d692f06bdac760df3162b6feac254739
Rafa Guerlak <guerlak@hotmail.com>
2024-03-31 00:14:50 -0300

    eventos

--- app/components/Agenda.js
+++ app/components/Agenda.js
@@ -1,10 +1,9 @@
 function Agenda() {
   return (
     <>
-      <h2>Small Riders, RJ - 04/04</h2>
-      <h2>Jack London, RJ - 14/04</h2>
-      <h2>Calabouço, RJ - 21/04</h2>
-      <h2>The Porter House, Dublin- 27/04</h2>
+      <h2>Evento Privado, RJ - 04/04</h2>
+      <h2>Evento Privado, RJ - 19/04</h2>
+      <h2>Evento Privado, RJ - 26/04</h2>
     </>
   );
 }
