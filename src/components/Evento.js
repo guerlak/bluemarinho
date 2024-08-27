c1e63254033548ca53255852db813c489c06589e
Rafa Guerlak <guerlak@hotmail.com>
2024-08-27 00:48:51 -0300

    fix: link off evento

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -3,15 +3,12 @@ import Link from "next/link";
 export default function Evento({ data, local, nome, slug }) {
   return (
     <div className="w-[22em] m-auto">
-      <Link
-        className="border p-4 rounded-md mt-8 flex divide-x-2 justify-center"
-        href={`eventos/${slug}`}
-      >
+      <div className="border p-4 rounded-md mt-8 flex divide-x-2 justify-center">
         <div className="text-orange-400">{data}</div>
         <div className="ml-2 px-2">
           {nome}, {local}
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
