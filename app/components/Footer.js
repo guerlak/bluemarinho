2dc9fb6a056e55fdf463af18d7a8bc268fd7b809
Rafa Guerlak <guerlak@hotmail.com>
2024-09-10 00:10:51 -0300

    fix: footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,14 +4,17 @@ import SocialIcons from "./SocialIcons";
 
 export default function Footer() {
   return (
-    <div className="flex justify-around my-10">
-      <div className="w-80">
-        <p>CONTATO PARA SHOWS</p>
-        <p>Filipe: (21) 98844-4059</p>
-        <p>bandabluemarinho@gmail.com</p>
-      </div>
+    <div className="flex justify-around items-center my-10 flex-col sm:flex-row ">
+      <SocialIcons size={50} className="w-80" />
 
-      <SocialIcons size={40} className="w-80" />
+      <div className="mt-4">
+        <div className="text-center text-gray-300">
+          <p>CONTATO PARA SHOWS</p>
+          <p>Filipe: (21) 98844-4059</p>
+          <p>bandabluemarinho@gmail.com</p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
