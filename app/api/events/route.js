5466b024f1be81402349640b832397552b2e99ed
Rafa Guerlak <guerlak@hotmail.com>
2024-09-21 12:18:23 -0300

    update event date

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -3,13 +3,6 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
   const data = [
     {
-      local: "Vg.Pequena-RJ",
-      nome: "Ziege Zag",
-      data: "14/09",
-      slug: "zz-14",
-      url: "https://www.instagram.com/ziegezag/",
-    },
-    {
       local: "Vg. Pequena-RJ",
       nome: "ZZ Festival",
       data: "26/10",
