a319b3785aec8bc45c184d74017b470f55883b2d
Rafael Leite <guerlak@MacBook-Air-som-tillhor-Rafael.local>
2025-03-31 23:23:12 -0300

    https

--- app/components/Agenda.js
+++ app/components/Agenda.js
@@ -6,7 +6,7 @@ async function Agenda() {
   // data from file
   //const file = await fs.readFile(process.cwd() + "/app/data.json", "utf8");
 
-  const data = await fetch("http://bluemarinho.com.br/api/events", {
+  const data = await fetch("https://bluemarinho.com.br/api/events", {
     next: { revalidate: 60 },
     cache: "no-store",
   });
