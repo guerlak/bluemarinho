2dba6628199a490c58192b304620a86916036afe
Rafael Leite <guerlak@MacBook-Air-som-tillhor-Rafael.local>
2025-04-01 17:00:56 -0300

    agenda update

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -3,12 +3,19 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const data = [
     {
+      local: "Botafogo-RJ",
+      nome: "Macaco Caolho",
+      data: "25/04",
+      slug: "macaco",
+      url: "https://www.instagram.com/macacocaolhopub/",
+    },
+    {
       local: "São Conrado-RJ",
       nome: "Quiqui",
       data: "26/04",
       slug: "quiqui",
       url: "https://www.instagram.com/quiquirio/",
-    },
+    }
   ];
 
   return NextResponse.json(data);
