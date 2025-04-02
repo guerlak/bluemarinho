f17686a864724ceb6fb1b947f8785a5d08c6ae57
Rafael Leite <guerlak@MacBook-Air-som-tillhor-Rafael.local>
2025-04-02 02:06:10 -0300

    fix mobile layout

--- app/components/Evento.js
+++ app/components/Evento.js
@@ -2,11 +2,11 @@
 
 export default function Evento({ data, local, nome, slug, url }) {
   return (
-    <div className="w-[500px] m-auto">
+    <div className="w-[30em] m-auto">
       <a href={url} target="blank" className="max-w-md m-auto">
         <div className="border p-4 rounded-md mt-8 flex">
-          <div className="text-orange-400 w-[140px] text-center border-r-2 p-2">{data}</div>
-          <div className="w-[400px] flex justify-center items-center text-center">
+          <div className="text-orange-400 w-[7em] text-center border-r-2">{data}</div>
+          <div className="w-[23em] flex justify-center items-center text-center">
             {nome}, {local}
           </div>
         </div>
